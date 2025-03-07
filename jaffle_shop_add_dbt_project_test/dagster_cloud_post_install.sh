a259f62fe9cd60309eaaddbb62b0fe64482d678c
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-03-07 11:57:17 -0500

    test to see prior dir files

--- jaffle_shop_add_dbt_project_test/dagster_cloud_post_install.sh
+++ jaffle_shop_add_dbt_project_test/dagster_cloud_post_install.sh
@@ -5,9 +5,12 @@ echo "Starting dagster_cloud_post_install.sh"
 
 ls -lah
 
-echo "Prepare DBT project for deployment"
-python -m pip install pip --upgrade
-pip install . --upgrade --upgrade-strategy eager
-dagster-dbt project prepare-and-package --file jaffle_shop_add_dbt_project_test/project.py
+echo "Files in preivious directory"
+ls -lah ../
+
+# echo "Prepare DBT project for deployment"
+# python -m pip install pip --upgrade
+# pip install . --upgrade --upgrade-strategy eager
+# dagster-dbt project prepare-and-package --file jaffle_shop_add_dbt_project_test/project.py
 
 echo "Post-install script completed successfully."
