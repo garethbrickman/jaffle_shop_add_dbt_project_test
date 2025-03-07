ea3b3d8222c6469ef9071712ddf69b0f41b4f818
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-03-07 09:52:18 -0500

    try post-install script

---
+++ jaffle_shop_add_dbt_project_test/dagster_cloud_post_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "Starting dagster_cloud_post_install.sh"
+
+ls -lah
+
+echo "Prepare DBT project for deployment"
+python -m pip install pip --upgrade
+# cd project-repo/${{ env.DAGSTER_PROJECT_NAME }}
+pip install . --upgrade --upgrade-strategy eager
+dagster-dbt project prepare-and-package --file ${{ env.DAGSTER_PROJECT_NAME }}/project.py
+
+
+echo "Post-install script completed successfully."
