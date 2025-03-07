67dba7eeade99ec144fb45d3da6ab899fd701a87
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-03-07 09:29:54 -0500

    add ls -lah to script

--- jaffle_shop_add_dbt_project_test/dagster_cloud_pre_install.sh
+++ jaffle_shop_add_dbt_project_test/dagster_cloud_pre_install.sh
@@ -3,14 +3,16 @@ set -e
 
 echo "Starting dagster_cloud_pre_install.sh..."
 
-# Update package lists
-apt-get update
+ls -lah
 
-echo "Prepare DBT project for deployment"
-python -m pip install pip --upgrade
-cd project-repo/${{ env.DAGSTER_PROJECT_NAME }}
-pip install . --upgrade --upgrade-strategy eager
-dagster-dbt project prepare-and-package --file ${{ env.DAGSTER_PROJECT_NAME }}/project.py
+# Update package lists
+# apt-get update
+
+# echo "Prepare DBT project for deployment"
+# python -m pip install pip --upgrade
+# cd project-repo/${{ env.DAGSTER_PROJECT_NAME }}
+# pip install . --upgrade --upgrade-strategy eager
+# dagster-dbt project prepare-and-package --file ${{ env.DAGSTER_PROJECT_NAME }}/project.py
 
 
 echo "Pre-install script completed successfully."
