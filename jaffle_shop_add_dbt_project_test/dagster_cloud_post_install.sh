45db6039e00adfe4401b6b6b0cf03982e62884f2
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-03-07 10:30:01 -0500

    remove cd, already runs in the correct dir

--- jaffle_shop_add_dbt_project_test/dagster_cloud_post_install.sh
+++ jaffle_shop_add_dbt_project_test/dagster_cloud_post_install.sh
@@ -7,9 +7,7 @@ ls -lah
 
 echo "Prepare DBT project for deployment"
 python -m pip install pip --upgrade
-cd ${DAGSTER_PROJECT_NAME}
 pip install . --upgrade --upgrade-strategy eager
 dagster-dbt project prepare-and-package --file ${DAGSTER_PROJECT_NAME}/project.py
-
 
 echo "Post-install script completed successfully."
