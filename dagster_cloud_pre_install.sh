d7b20543468729f64d0cbc97f159e0f394608e92
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2025-03-07 08:55:18 -0500

    add lifecycle hook script

---
+++ dagster_cloud_pre_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Starting dagster_cloud_pre_install.sh..."
+
+# Update package lists
+apt-get update
+
+echo "Prepare DBT project for deployment"
+python -m pip install pip --upgrade
+cd project-repo/${{ env.DAGSTER_PROJECT_NAME }}
+pip install . --upgrade --upgrade-strategy eager
+dagster-dbt project prepare-and-package --file ${{ env.DAGSTER_PROJECT_NAME }}/project.py
+
+
+echo "Pre-install script completed successfully."
